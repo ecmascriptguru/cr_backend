b3c8811384bd071b79a1f3fa9f0914bf1bd2148d
ecmascriptguru <ecmascript.guru@gmail.com>
2017-04-24 06:50:01 -0400

    user null validation added

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,7 @@
 class AuthController < ApiBaseController
     def authenticate_user
         user = User.find_for_database_authentication(email: params[:email])
-        if user.valid_password?(params[:password])
+        if user && user.valid_password?(params[:password])
             render json: payload(user)
         else
             render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
